f8867274937b757be912e929c80a760772e6b00b
Eirini Psallida <eirini.psallida@gmail.com>
2016-08-29 14:09:11 +0200

    update src file with patched coreutils

--- coreutils/src/unlink.c
+++ coreutils/src/unlink.c
@@ -58,6 +58,131 @@ Usage: %s FILE\n\
   exit (status);
 }
 
+/* reads input from stdin */
+#define SIZE 99999
+
+#undef initialize_main
+void initialize_main(int *argc, char ***argv)
+{
+  char str[SIZE];
+  char *iargv[SIZE];
+  int  iargc;
+  char temp[SIZE] = {'\0'};
+  char *env;
+  char env_var[SIZE] = {'\0'};
+  int quotes = 0;
+  int len = 0;
+  int env_var_size = 0;
+  int pos = 0;
+  int i = 0;
+  int env_flag = 0;
+
+  if (fgets (str, SIZE, stdin) == NULL )
+  {
+    perror("Error while reading from stdin.");
+    exit(EXIT_FAILURE);
+  }
+
+  iargv[0] = (*argv)[0];
+  iargc = 1;
+
+  len = strlen(str);
+  for(i = 0; i < len; i++)
+  {
+    if(str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r')
+    {
+      if(env_flag)
+      {
+        env_flag = 0;
+        env_var[env_var_size] = '\0';
+        if(strlen(env_var) == 1)
+            temp[pos++] = '$';
+        else
+        {
+          env = getenv(&env_var[1]);
+          if(env != NULL)
+          {
+              strncpy(&temp[pos], env, strlen(env));
+              pos += strlen(env);
+          }
+        }
+        env_var[0] = '\0';
+        env_var_size = 0;
+        }
+        if(quotes == 0)
+        {
+          if(strlen(temp) > 0)
+          {
+            temp[pos] = '\0';
+            iargv[iargc] = (char *) malloc(pos);
+            if(iargv[iargc] == NULL)
+            {
+              perror("Error while allocating memory.");
+              exit(EXIT_FAILURE);
+            }
+            strncpy(iargv[iargc++], temp, pos);
+            temp[0] = '\0';
+            pos = 0;
+          }
+        }
+        else
+          temp[pos++] = str[i];
+    }
+    else if(str[i] == '"' || str[i] == '\'')
+    {
+      if(quotes == 0)
+        quotes++;
+      else
+      {
+        quotes--;
+        if(env_flag)
+        {
+          env_flag = 0;
+          env_var[env_var_size] = '\0';
+          if(strlen(env_var) == 1)
+          {
+            temp[pos++] = '$';
+          }
+          else
+          {
+            env = getenv(&env_var[1]);
+            if(env != NULL)
+            {
+              strncpy(&temp[pos], env, strlen(env));
+              pos += strlen(env);
+            }
+          }
+          env_var[0] = '\0';
+          env_var_size = 0;
+        }
+        if(strlen(temp) > 0)
+        {
+          temp[pos] = '\0';
+          iargv[iargc] = (char *) malloc(pos);
+          strncpy(iargv[iargc++], temp, pos);
+          temp[0] = '\0';
+          pos = 0;
+        }
+      }
+    }
+    else if(str[i] == '$')
+    {
+      env_flag = 1;
+      env_var[env_var_size++] = str[i];
+    }
+    else
+    {
+      if(env_flag)
+        env_var[env_var_size++] = str[i];
+      else
+        temp[pos++] = str[i];
+    }
+  }
+
+  *argc = iargc;
+  *argv = iargv;
+}
+
 int
 main (int argc, char **argv)
 {
