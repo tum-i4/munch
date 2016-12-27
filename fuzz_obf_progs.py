a55696912b2060f2140260beb981399127708bf1
Saahil Ognawala <saahil.in@gmail.com>
2016-12-27 13:45:41 +0100

    also used glob.glob - similar to patch_obf_progs

--- fuzz_obf_progs.py
+++ fuzz_obf_progs.py
@@ -2,6 +2,7 @@
 
 import os, sys
 import subprocess, time, signal
+import glob
 
 def main(argv):
  
@@ -15,40 +16,40 @@ def main(argv):
         raise
         
     sync_dir = "sync_dir" # the root folder where all instances will store their results
-    if not os.path.exists(sync_dir):
-        os.makedirs(sync_dir)
+    if not os.path.exists(obf_dir + "/" + sync_dir):
+        os.makedirs(obf_dir + "/" + sync_dir)
     
     counter = 1
     pids = []
-    for filename in os.listdir(obf_dir):
+    for filename in glob.glob(obf_dir+"/*_afl.c"):
         if not filename.startswith('.' or '..'):
-        # 1. compile the patched obfuscated prog with afl-gcc
-        afl_binary = filename[:-2] + "_bin"
-        args = ["afl-gcc", filename, "-o", afl_binary] # creates the instrumented binary to fuzz with afl
-        subprocess.call(args)
-
-        # 2. run the afl binary with afl-fuzz
-        target_sync_dir = sync_dir + "/" + filename
-        fuzzed_binary = "./" + afl_binary
-        fuzzed_instance = filename + str(counter) #sync id of the instance
-        # master
-        args = ["afl-fuzz", "-i", testcases, "-o", target_sync_dir, "-M", fuzzed_instance, fuzzed_binary]
-        proc = subprocess.Popen(args)
-        pids.append(proc.pid)
-
-        for i in range(1, num_of_cores):
-            counter += 1
-            fuzzed_instance = filename + str(counter)
-            # slaves
-            args = ["afl-fuzz", "-i", testcase_dir, "-o", target_sync_dir, "-S", fuzzed_instance, fuzzed_binary]
+            # 1. compile the patched obfuscated prog with afl-gcc
+            afl_binary = filename[:-2] + "_bin"
+            args = ["afl-gcc", filename, "-o", afl_binary] # creates the instrumented binary to fuzz with afl
+            subprocess.call(args)
+    
+            # 2. run the afl binary with afl-fuzz
+            target_sync_dir = os.path.dirname(filename) + "/" + sync_dir + "/" + os.path.basename(filename[:-2])
+            fuzzed_binary = afl_binary
+            fuzzed_instance = str(counter) #sync id of the instance
+            # master
+            args = ["afl-fuzz", "-i", testcases, "-o", target_sync_dir, "-M", fuzzed_instance, fuzzed_binary]
             proc = subprocess.Popen(args)
             pids.append(proc.pid)
 
-        time.sleep(fuzz_time) # suspend the main process 
-        for pid in pids:
-            os.kill(pid, signal.SIGKILL) # kill all the subprocesses generated and fuzz the next program
-        pids = []
-        counter = 1
+            for i in range(1, num_of_cores):
+                counter += 1
+                fuzzed_instance = str(counter)
+                # slaves
+                args = ["afl-fuzz", "-i", testcases, "-o", target_sync_dir, "-S", fuzzed_instance, fuzzed_binary]
+                proc = subprocess.Popen(args)
+                pids.append(proc.pid)
+
+            time.sleep(fuzz_time) # suspend the main process 
+            for pid in pids:
+                os.kill(pid, signal.SIGKILL) # kill all the subprocesses generated and fuzz the next program
+            pids = []
+            counter = 1
     
 if __name__ == '__main__':
     main(sys.argv[1:])
