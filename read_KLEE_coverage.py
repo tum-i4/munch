ae0bd23afbcef6abbdb34fd0247c72a9c360395d
hutoTUM <hutoTUM@users.noreply.github.com>
2017-04-10 15:23:00 +0200

    Add verbosity flag and better file name handling

--- read_KLEE_coverage.py
+++ read_KLEE_coverage.py
@@ -1,38 +1,42 @@
 from helper import get_flat_inversed_topology
 import argparse
-import os
+from os import path
 import time
-import glob
+from glob import glob
 
 
-def main(bcfilename):
+def main(bcfilename, verbose=False):
     # get a list of functions topologically ordered
     all_funcs_topologic = list(get_flat_inversed_topology(bcfilename))
-    print(all_funcs_topologic)
-    print("TOTAL FUNCS :", len(all_funcs_topologic))
+    if verbose:
+        print("All functions:", all_funcs_topologic)
 
     covered_from_klee = set()
-    pos = bcfilename.rfind('/')
-    klee_cov_funcs = bcfilename[:pos + 1] + "covered_funcs.txt"
-    klee_uncov_funcs = bcfilename[:pos + 1] + "uncovered_funcs.txt"
-
-    for filename in glob.glob(bcfilename[:pos + 1] + "klee-out-*"):
-        klee_dir = os.path.abspath(filename) + "/run.istats"
-        print(klee_dir)
-        f = open(klee_dir, "r")
+    for filename in glob(path.join(path.dirname(bcfilename), "klee-out-*")):
+        runistatsfile = path.join(path.abspath(filename), "run.istats")
+        print(runistatsfile)
+        f = open(runistatsfile, "r")
         for line in f:
             if line[:4] == "cfn=":
                 covered_from_klee.add(line[4:-1])
 
-    print(len(covered_from_klee))
-    print(covered_from_klee)
+    if verbose:
+        print(covered_from_klee)
+
+    print("KLEE covers {:d} out of {:d} total functions ({:.1%})".format(
+        len(covered_from_klee), len(all_funcs_topologic),
+        len(covered_from_klee) / len(all_funcs_topologic)))
+
+    klee_cov_funcs = path.join(path.dirname(bcfilename), "covered_funcs.txt")
+    klee_unc_funcs = path.join(path.dirname(bcfilename), "uncovered_funcs.txt")
+
     cov_file = open(klee_cov_funcs, 'w+')
-    uncov_file = open(klee_uncov_funcs, 'w+')
+    unc_file = open(klee_unc_funcs, 'w+')
     for func in all_funcs_topologic:
         if func in covered_from_klee:
             cov_file.write("%s\n" % func)
         else:
-            uncov_file.write("%s\n" % func)
+            unc_file.write("%s\n" % func)
 
     return 1
 
@@ -49,8 +53,13 @@ if __name__ == '__main__':
         type=argparse.FileType('r'),
         help="Bitcode file, that was analyzed with KLEE"
     )
+    parser.add_argument(
+        "-v", "--verbose",
+        help="Increase output verbosity",
+        action="store_true"
+    )
 
     args = parser.parse_args()
 
-    main(args.bcfile.name)
+    main(args.bcfile.name, args.verbose)
 
